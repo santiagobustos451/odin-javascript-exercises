19dde6d582ada7eb49b9acdf5251ebf3b339cb78
Santiago Bustos <santiagobustos451@gmail.com>
2023-07-29 14:12:40 -0300

    Ex 4 Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    elements.forEach(element => {
+        indexToRemove = array.indexOf(element);
+        if(indexToRemove != -1){
+            array.splice(indexToRemove, 1);
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
