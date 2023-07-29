ee23611d9b315a48a659a334d8e11fd733b2fd00
Santiago Bustos <santiagobustos451@gmail.com>
2023-07-29 15:45:41 -0300

    Ex 10 Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0)return "OOPS";
 
+    f = [];
+    for(let i = 0; i < n; i++){
+        nextNumber = f.length >= 2 ? f[f.length-1] + f[f.length-2] : 1;
+        f.push(nextNumber);
+    }
+    return f[n-1];
 };
 
 // Do not edit below this line
