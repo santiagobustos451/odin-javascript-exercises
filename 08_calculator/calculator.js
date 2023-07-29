9e20a31bf5e35ffdf209a06593fb6b036a8516aa
Santiago Bustos <santiagobustos451@gmail.com>
2023-07-29 15:06:09 -0300

    Ex 8 Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	result = 0;
+  array.forEach(element => {
+    result += element;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	result = 1;
+  array.forEach(element => {
+    result *= element;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  result = 1;
+	for(let i = 2; i <= a; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
