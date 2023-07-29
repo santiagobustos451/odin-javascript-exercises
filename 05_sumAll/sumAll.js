dc5450bf11d93feaeb31112c77ba7cc61cd2ee84
Santiago Bustos <santiagobustos451@gmail.com>
2023-07-29 14:40:02 -0300

    Ex 5 Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a < 0 || typeof a != "number"|| b < 0 || typeof b != "number") return "ERROR";
 
+    let [start,end] = a < b ? [a,b]:[b,a];
+    let result = 0;
+    for(let i = start; i <= end; i++){
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
