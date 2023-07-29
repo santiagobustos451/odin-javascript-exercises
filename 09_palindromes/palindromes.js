a937cea08c0b03c2035849dcd94d64b82bfeabe6
Santiago Bustos <santiagobustos451@gmail.com>
2023-07-29 15:22:55 -0300

    Ex 9 Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const reverseString = function (string) {
+    return string.split("").reverse().join("");
+  };
 
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[\W_]/g, '');
+    revString = reverseString(string);
+    return string === revString;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
