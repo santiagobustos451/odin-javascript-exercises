01369f47ee0f8aa3a7b152163c48e6b8306a84c7
Santiago Bustos <santiagobustos451@gmail.com>
2023-07-30 12:31:42 -0300

    All exercises done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        return (findAge(oldest) > findAge(currentPerson))? oldest:currentPerson;
+    }) 
 };
+
+const findAge = function(person){
+    if(!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear();
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
